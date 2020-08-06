37e94dbb8dc16fd6e856331497eabb8cc753ffef
Kyle Mattimore <kmattimore@onenorth.com>
2020-08-06 15:28:44 -0500

    add option to fallback to req.ip for validation, if the expected client ip header is not set for a request

--- middleware/anonymousAccessBlocker.js
+++ middleware/anonymousAccessBlocker.js
@@ -14,6 +14,10 @@ module.exports = function() {
  
         // Process anonymous requests.
         if (!req.user || !req.user.canAccessKeystone) {
+
+            if(process.env.ANONYMOUS_ACCESS_BLOCKER_DEBUG === 'true'){
+                console.log(req.headers)
+            }
             
             // Check for IP range allowances.  Requests will be allowed through if the IP address is in range.
             var ipRanges = process.env.ANONYMOUS_ACCESS_BLOCKER_ALLOWED_IP_RANGES;
@@ -21,8 +25,26 @@ module.exports = function() {
                 // The set of allowed ranges has to be separated by space characters, a comma, or newline.
                 var allowedRanges = ipRanges.split(/\s+|,|\n/);
                 
-                //if CLIENT_IP_ADDRESS_HEADER is set and a request coming in does not contain it, it will be denied regardless of IP
-                var requestIP = (process.env.CLIENT_IP_ADDRESS_HEADER) ? req.header(process.env.CLIENT_IP_ADDRESS_HEADER) : req.ip;
+                // The ideal case: only allow traffic that can prove it passed through your cloud waf provider first, before [heroku's] reverse proxy. 
+                // May require using an unguessable app name + obscured dns to prevent <appname>.herokuapp.com access
+                // Set CLIENT_IP_ADDRESS_HEADER in [heroku]
+                var requestIP = '';
+                var requestIPSource = 'req.ip'
+                if(process.env.CLIENT_IP_ADDRESS_HEADER) {
+                    requestIP = req.header(process.env.CLIENT_IP_ADDRESS_HEADER)
+                    requestIPSource = process.env.CLIENT_IP_ADDRESS_HEADER
+
+                    //fallback to trust-proxy - only if configured
+                    if(!requestIP && process.env.ANONYMOUS_ACCESS_BLOCKER_ENFORCE_HEADER === 'false') {
+                        requestIP = req.ip;
+                        requestIPSource = 'fallback req.ip'
+                    }
+                }
+                // Less ideal: fallback to express 'trust-proxy' setting
+                else {
+                    requestIP = req.ip;
+                }
+
                 requestIP = rangeCheck.searchIP(requestIP);
                 
                 // Deny the request if request IP is not in one of the allowed
@@ -31,13 +53,16 @@ module.exports = function() {
                 
                 if (requestAllowed) {
                     // Allow the request to process
-                    console.log('keystone-hosting: Allowed IP ' + requestIP);
+                    console.log('keystone-hosting: Allowed [' + requestIPSource + '] IP ' + requestIP);
                     return next();
                 }
             }
+            else {
+                console.error('keystone-hosting: IP restriction enabled with no allowed IP ranges!')
+            }
 
             // Request is not allowed.  Send the contents of the unauthorized.html file.
-            console.log('keystone-hosting: Blocked IP ' + requestIP);
+            console.log('keystone-hosting: Blocked [' + requestIPSource + '] IP ' + requestIP);
             
             //set 'unauthorized' response code
             res.status(401)
